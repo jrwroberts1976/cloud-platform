4a78632e3747d3e6ea595d0712b4702fc868914a
James Roberts <jrwroberts1976@gmail.com>
2026-07-18 06:45:25 +0100

    Deploy AWS VPC networking foundation

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -1,9 +1,43 @@
 variable "vpc_cidr" {
+
   description = "CIDR range for VPC"
-  type        = string
+
+  type = string
+
 }
 
+
+variable "public_subnet_cidr" {
+
+  description = "Public subnet CIDR"
+
+  type = string
+
+}
+
+
+variable "private_subnet_cidr" {
+
+  description = "Private subnet CIDR"
+
+  type = string
+
+}
+
+
+variable "availability_zone" {
+
+  description = "AWS availability zone"
+
+  type = string
+
+}
+
+
 variable "environment" {
+
   description = "Environment name"
-  type        = string
+
+  type = string
+
 }
