b25793f4727aff866706fbb6e90752d538acc4df
James Roberts <jrwroberts1976@gmail.com>
2026-07-18 06:54:10 +0100

    changed subnets

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,7 +1,34 @@
+
 output "environment" {
+
   value = var.environment
+
 }
 
+
 output "region" {
+
   value = var.aws_region
+
+}
+
+
+output "vpc_id" {
+
+  value = module.networking.vpc_id
+
+}
+
+
+output "public_subnet_id" {
+
+  value = module.networking.public_subnet_id
+
+}
+
+
+output "private_subnet_id" {
+
+  value = module.networking.private_subnet_id
+
 }
