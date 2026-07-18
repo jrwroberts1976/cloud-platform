606da7b28daca535d27cb2c8574c8f2b2da83feb
James Roberts <jrwroberts1976@gmail.com>
2026-07-18 06:44:46 +0100

    Deploy AWS VPC networking foundation

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,7 +1,15 @@
 module "networking" {
+
   source = "../../modules/networking"
 
   vpc_cidr = "10.0.0.0/16"
 
+  public_subnet_cidr = "10.0.1.0/24"
+
+  private_subnet_cidr = "10.0.2.0/24"
+
+  availability_zone = "eu-west-2a"
+
   environment = var.environment
+
 }
